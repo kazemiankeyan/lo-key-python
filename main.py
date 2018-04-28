65927d836ea9ed387cd4a119efd2d8bdf55c3f17
Dilraj <dilrajsinghdevgun@hotmail.co.uk>
2018-04-28 18:15:31 +0200

    changed login flow

--- main.py
+++ main.py
@@ -2,43 +2,22 @@ import os
 import sys
 import json
 import spotipy
-# import webbrowser
 import spotipy.util as util
-# from json.decoder import JSONDecodeError
-
-# # Get the username from terminal
-# username = sys.argv[1]
-#
-# # username: kaaazemian
-#
-# try:
-#     token = util.prompt_for_user_token(username)
-# except:
-#     os.remove(f".cache-{username}")
-
-# export SPOTIPY_CLIENT_ID='5155e553917b4dccafe00786c41692e7'
-# export SPOTIPY_CLIENT_SECRET='5155e553917b4dccafe00786c41692e7'
-# export SPOTIPY_REDIRECT_URI='http://google.com/'
+from spotipy.oauth2 import SpotifyClientCredentials
 
 def main():
-    print('hello')
-    scope = 'user-library-read'
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        print "Usage: %s username" % (sys.argv[0],)
-        sys.exit()
-    token = util.prompt_for_user_token(username, scope)
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        results = sp.current_user_saved_tracks()
-        for item in results['items']:
-            track = item['track']
-            print track['name'] + ' - ' + track['artists'][0]['name']
-    else:
-        print "Can't get token for", username
+    client_credentials_manager = SpotifyClientCredentials(client_id='9e1915c12f364fc599912bafb773ab28', client_secret='5155e553917b4dccafe00786c41692e7')
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+    playlists = sp.user_playlists('kaaazemian')
 
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
 
 if __name__ == '__main__':
     main()
