faab421485e6a5c39118512cf4ae9651cbd73bb6
Dilraj <dilrajsinghdevgun@hotmail.co.uk>
2018-05-10 11:44:05 +0200

    added ios app

---
+++ Lo-Key/Lo-Key/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lo-Key
+//
+//  Created by Dilraj Devgun on 5/10/18.
+//  Copyright © 2018 Dilraj Devgun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lo-Key/Lo-Key/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lo-Key
+//
+//  Created by Dilraj Devgun on 5/10/18.
+//  Copyright © 2018 Dilraj Devgun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
