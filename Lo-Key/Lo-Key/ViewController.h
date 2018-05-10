6f6d3f1a11cc4d1a37ef79dd712411421eb89bb3
Keyan <kazemiankeyan@gmail.com>
2018-05-10 12:34:30 +0200

    added a search that includes all three algorithms

---
+++ Lo-Key/Lo-Key/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lo-Key
+//
+//  Created by Dilraj Devgun on 5/10/18.
+//  Copyright © 2018 Dilraj Devgun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lo-Key/Lo-Key/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lo-Key
+//
+//  Created by Dilraj Devgun on 5/10/18.
+//  Copyright © 2018 Dilraj Devgun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
